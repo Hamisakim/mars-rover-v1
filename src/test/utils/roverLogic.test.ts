3cfea4ac6b38e129617ea4c7fab6300b0844a8da
Hamisakim <76621344+Hamisakim@users.noreply.github.com>
2024-07-15 12:15:30 +0100

    chore: added basic skeleton

---
+++ src/test/utils/roverLogic.test.ts
@@ -0,0 +1,23 @@
+describe('Rover Logic', () => {
+  describe('validateInstructions', () => {
+    it('should validate a string of instructions correctly', () => {});
+  });
+
+  describe('moveRover', () => {
+    it('should move the rover forward when facing north', () => {});
+
+    it('should move the rover forward when facing east', () => {});
+
+    it('should move the rover forward when facing south', () => {});
+
+    it('should move the rover forward when facing west', () => {});
+  });
+
+  describe('rotateRover', () => {
+    it('should rotate the rover 90 degrees to the right', () => {});
+
+    it('should rotate the rover 90 degrees to the left', () => {});
+  });
+});
+
+export {};
